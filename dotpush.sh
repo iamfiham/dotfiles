ea1c3054f271b7145fc537f6f2a0cc8394d878eb
Mohamed Fiham <xyzfiham@gmail.com>
2025-05-21 19:13:46 +0530

    dot file push

--- dotpush.sh
+++ dotpush.sh
@@ -3,33 +3,40 @@
 DOTFILES_DIR="$HOME/.dotfiles"
 BRANCH=main
 
+# colors
+BG_COLOR="\033[48;2;52;204;252m"
+FG_BLACK="\033[01;30m"
+
+RESET="\033[00m"
+
 # Go to dotfiles directory
 cd "$DOTFILES_DIR" || {
-    echo "[ERROR] Dotfiles directory not found at $DOTFILES_DIR"
+    echo -e "\n${BG_COLOR}${FG_BLACK}[ERROR]${RESET} Dotfiles directory not found at $DOTFILES_DIR"
     exit 1
 }
 
 # Show git status
-echo -e "\n[INFO] Git status:"
+echo -e "\n${BG_COLOR}${FG_BLACK}[INFO]${RESET} Git status:"
 git status
 
 # Add all changes
-echo -e "\n[INFO] Staging files..."
+echo -e "\n${BG_COLOR}${FG_BLACK}[INFO]${RESET} Staging files..."
 git add .
 
 # Commit with user message
-echo -e "\n[INFO] Commiting files..."
+echo -e "\n${BG_COLOR}${FG_BLACK}[INFO]${RESET} Commiting files..."
 read -rp "Enter commit message: " COMMIT_MSG
 
 if ! git commit -m "$COMMIT_MSG"; then
-    echo -e "\n[WARNING] Nothing to commit or commit failed."
+    echo -e "\n${BG_COLOR}${FG_BLACK}[WARNING]${RESET} Nothing to commit or commit failed."
 fi
 
 # Push to remote
-echo -e "\n[INFO] Pushing to origin/$BRANCH..."
+echo -e "\n${BG_COLOR}${FG_BLACK}[INFO]${RESET} Pushing to origin/$BRANCH..."
+
 if git push origin "$BRANCH"; then
-    echo -e "\n[SUCCESS] Dotfiles pushed to GitHub."
+    echo -e "\n${BG_COLOR}${FG_BLACK}[SUCCESS]${RESET} Dotfiles pushed to GitHub."
 else
-    echo -e "\n[ERROR] Git push failed. Check your network or authentication."
+    echo -e "\n${BG_COLOR}${FG_BLACK}[ERROR]${RESET} Git push failed."
     exit 1
 fi
