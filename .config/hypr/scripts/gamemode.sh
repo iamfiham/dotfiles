fabafa93f4eac06a521a0c412d6e962cfd89fd6a
Mohamed Fiham <xyzfiham@gmail.com>
2025-06-13 14:50:03 +0530

    changes

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -6,7 +6,6 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword decoration:shadow:enabled 0;\
         keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
     exit
