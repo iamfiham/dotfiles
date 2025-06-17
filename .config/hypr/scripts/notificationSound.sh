6b8682e26399e90b04165bb7e92d36d1cb8dcfb8
Mohamed Fiham <xyzfiham@gmail.com>
2025-06-18 00:29:06 +0530

    mako and waybar config

--- .config/hypr/scripts/notificationSound.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-dbus-monitor "interface='org.freedesktop.Notifications'" |
-while read -r line; do
-    echo "$line" | grep -q "method call.*Notify" && paplay /usr/share/sounds/freedesktop/stereo/bell.oga
-done
